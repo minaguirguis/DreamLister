0f16abb430667b93c8f79c0cf4adad41ffa2cb6b
Mina Guirguis <MGuirguis.10@gmail.com>
2017-12-23 01:02:40 -0500

    We added the feature where the user can add items to the list as well as edit existing items

--- DreamLister/Controller/ItemDetailsVC.swift
+++ DreamLister/Controller/ItemDetailsVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate  {
+class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
 
     @IBOutlet weak var storePicker: UIPickerView!
     @IBOutlet weak var titleField: CustomTextField!
@@ -17,6 +17,8 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
     @IBOutlet weak var detailsField: CustomTextField!
     
     var stores = [Store]()
+    var itemToEdit: Item?
+    //made it optional by putting "?"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,22 +30,27 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
         storePicker.delegate = self
         storePicker.dataSource = self
         
-//        let store = Store(context: context)
-//        store.name = "Best Buy"
-//        let store2 = Store(context: context)
-//        store2.name = "Tesla Dealership"
-//        let store3 = Store(context: context)
-//        store3.name = "Frys Electronics"
-//        let store4 = Store(context: context)
-//        store4.name = "Target"
-//        let store5 = Store(context: context)
-//        store5.name = "Amazon"
-//        let store6 = Store(context: context)
-//        store6.name = "K Mart"
-//
-//        ad.saveContext()
+        let store = Store(context: context)
+        store.name = "Best Buy"
+        let store2 = Store(context: context)
+        store2.name = "Tesla Dealership"
+        let store3 = Store(context: context)
+        store3.name = "Frys Electronics"
+        let store4 = Store(context: context)
+        store4.name = "Target"
+        let store5 = Store(context: context)
+        store5.name = "Amazon"
+        let store6 = Store(context: context)
+        store6.name = "K Mart"
+
+        ad.saveContext()
         
         getStores()
+        
+        if itemToEdit != nil {
+            
+            loadItemData()
+        }//if we have an item to edit we will load that item's data
         
         
 
@@ -77,5 +84,73 @@ class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDeleg
         }
         
     }
+    
+    @IBAction func savePressed(_ sender: UIButton) {
+        
+        var item: Item!
+        
+        if itemToEdit == nil {
+            
+            item = Item(context: context)
+        } else {
+            
+            item = itemToEdit
+        }
+        
+        
+        
+        //then we started assigning those variables
+        if let title = titleField.text {
+            item.title = title
+        }
+        
+        if let price = PriceField.text{
+           
+            item.price = (price as NSString).doubleValue
+        }
+        if let details = detailsField.text {
+            
+            item.details = details
+        }
+        
+        item.toStore = stores[storePicker.selectedRow(inComponent: 0)] //assigning the store we selected
+        //we are assigning the "tostore" relationshhip to that certain item
+        //"in component" is not row, it is the column. Since we only have one column its zero
+       
+        
+        ad.saveContext()
+        
+        navigationController?.popViewController(animated: true)//to take us back to the Main screen after adding/editing an item
+    }
+    
+    func loadItemData() {
+        
+        if let item = itemToEdit { //did this so we dont have to write "itemToEdit" each time
+            
+            titleField.text = item.title
+            PriceField.text = "\(item.price)"
+            detailsField.text = item.details
+            
+            //what this loop does is that it loops through to see what the correct store is and assigns it to a different variable
+            if let store = item.toStore {//check to see if there is a store (item might not have one)
+                
+                var index = 0
+                repeat {
+                    let s = stores[index]
+                    if s.name == store.name {
+                        
+                        storePicker.selectRow(index, inComponent: 0, animated: false)
+                        
+                        break
+                    }
+                    index += 1
+                    
+                }while (index < stores.count)
+            }
+            
+        }
+        
+    }
+    
     
 }
--- DreamLister/Controller/MainVC.swift
+++ DreamLister/Controller/MainVC.swift
@@ -9,8 +9,8 @@
 import UIKit
 import CoreData
 
-class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate {
-
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate{
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var segment: UISegmentedControl!
     
@@ -22,12 +22,13 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         tableView.delegate = self
         tableView.dataSource = self
         
-        generateTestData()
+        //generateTestData()
         attemptFetch()
     }
-
+    
+    // functions
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
         configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
         return cell
@@ -38,13 +39,45 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         cell.configureCell(item: item)
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //making sure there are objects and that there is at least one
+        if let objs = controller.fetchedObjects , objs.count > 0 {
+         
+            let item = objs[indexPath.row]//setting item equal to the specific item in our list
+            performSegue(withIdentifier: "ItemDetailsVC", sender: item)//perform a segue and telling it that the identifier is the "ItemDetailsVC" and we are sending an the item
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        //making sure it is the segue we are looking for
+        if segue.identifier == "ItemDetailsVC" {
+            
+            //creating a variable called "destination" and setting it equal to "ItemDetailsVC"
+            if let destination = segue.destination as? ItemDetailsVC {
+                
+                //then creating and item and casting it as "Item" (which is sender)
+                if let item = sender as? Item {
+                    
+                    //we are saying that our destinationvc and the itemToEdit is what to assign it to
+                    destination.itemToEdit = item
+                }
+            }
+        }
+    }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if let sections = controller.sections {
             let sectionInfo = sections[section]
             return sectionInfo.numberOfObjects
         }
-        
         return 0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -54,93 +87,83 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         return 0
     }
     
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 150
-    }
-    
-    
     func attemptFetch() {
-        
-        
         let fetchRequest: NSFetchRequest<Item> = Item.fetchRequest()
         //we created a fetch request variable and told it what kind of item we will be fetching, and then we are telling it what to go fetch
-        let dateSort = NSSortDescriptor(key: "created", ascending: false)
+        let dateSort = NSSortDescriptor (key: "created", ascending: false)
         fetchRequest.sortDescriptors = [dateSort]//passing in the sort descriptor that we just created
         
-        let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        let controller = NSFetchedResultsController (fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+       
+        controller.delegate = self
         
         self.controller = controller //setting outside controller to inside controller
         
         do {
             try controller.performFetch()
-        }catch {
+        } catch {
             let error = error as NSError
-            print("\(error)")
-            
+            print ("\(error)")
         }
-        
-       func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>){
-            tableView.beginUpdates()
-        }
-        
-        func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>){
-            tableView.endUpdates()
-        }
-        
-        func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-            
-            switch(type) {
-                
-            case.insert://inserting cell
-                if let indexPath = newIndexPath {
-                    tableView.insertRows(at: [indexPath], with: .fade)
-                }
-                break
-            case.delete://deleting cell
-                if let indexPath = indexPath {
-                    tableView.deleteRows(at: [indexPath], with: .fade)
-                }
-                break
-            case .move://if the user is moving it we are deleting in current place then move
-                if let indexPath = indexPath {
-                    tableView.deleteRows(at: [indexPath], with: .fade)
-                }
-                if let indexPath = newIndexPath{
-                    tableView.insertRows(at: [indexPath], with: .fade)
-                }
-                break
-            case .update://selecting cell
-                if let indexPath = indexPath {
-                    let cell = tableView.cellForRow(at: indexPath) as! ItemCell
-                    configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
-                }
-                break
-            }
-        
     }
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch (type) {
+        case .insert: //inserting cell
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            
+        case.delete: //deleting cell
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            
+        case.update: //selecting cell
+            if let indexPath = indexPath {
+                let cell = tableView.cellForRow(at: indexPath) as! ItemCell
+                configureCell(cell: cell, indexPath: indexPath as IndexPath as NSIndexPath)
+            }
+            
+        case.move: //if the user is moving it we are deleting in current place then move
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            
+            if let indexPath = newIndexPath {
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            
+            break
+        }
     }//function to retrieve information from database
     
+    // generate test data
     
     func generateTestData() {
         let item = Item(context: context)
-        item.title = "Macbook Pro"
-        item.price = 1800
-        item.details = "I Cant wait the september event, I hope they release new MPS."
-        
+        item.title = "Tesla Model S"
+        item.price = 80000
+        item.details = "VROOM, VROOM!!"
         let item2 = Item(context: context)
-        item2.title = "Wireless Beats Studio"
-        item2.price = 300
-        item2.details = "But, its so nice to be able to block out everyone with the noise canceling feature."
-        
+        item2.title = "MacBook Pro"
+        item2.price = 1800
+        item2.details = "I can't wait until September event!"
         let item3 = Item(context: context)
-        item3.title = "Tesla Model S"
-        item3.price = 110000
-        item3.details = "One day I will own this car."
+        item3.title = "iMac"
+        item3.price = 2000
+        item3.details = "Can't wait to get this for my home office!"
         
         ad.saveContext()
     }
-    
-
 }
-
